574ad1a9c14ed188334e0d204189d5bb8d967374
ivan <belyakov_ivan96@mail.ru>
2024-07-11 18:57:24 +0300

    ElementCounter

---
+++ src/main/java/org/example/ElementCounter.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ElementCounter {
+
+    public static <T> Map<T, Integer> countElementsMap(T[] array) {
+        Map<T, Integer> resultMap = new HashMap<>();
+
+        for (T element : array) {
+            if (resultMap.containsKey(element)) {
+                resultMap.put(element, resultMap.get(element) + 1);
+            } else {
+                resultMap.put(element, 1);
+            }
+        }
+
+        return resultMap;
+    }
+
+    public static void main(String[] args) {
+        Integer[] numbers = {1, 2, 3, 1, 2, 3, 4, 5, 1, 2};
+        Map<Integer, Integer> countElementsMap = countElementsMap(numbers);
+
+        for (Map.Entry<Integer, Integer> entry : countElementsMap.entrySet()) {
+            System.out.println("Элемент: " + entry.getKey() + ", Количество: " + entry.getValue());
+        }
+    }
+}
